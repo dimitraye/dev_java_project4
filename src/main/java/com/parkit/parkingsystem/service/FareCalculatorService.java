6dab54e761931bf0a7c7539bfee3a0de76238983
dimitraye <dimitraye94@gmail.com>
2022-10-24 23:34:32 +0200

    Free 30-min parking

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -6,7 +6,8 @@ import com.parkit.parkingsystem.model.Ticket;
 import java.util.concurrent.TimeUnit;
 
 public class FareCalculatorService {
-
+    public final int FREE_TIME_THIRTY_MINS = 30;
+    public final int MINUTES_IN_HOUR = 60;
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
@@ -15,13 +16,18 @@ public class FareCalculatorService {
         long duration = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
         long durationInMinutes = TimeUnit.MILLISECONDS.toMinutes(duration);
 
+        if (durationInMinutes < FREE_TIME_THIRTY_MINS) {
+          ticket.setPrice(Fare.RATE_UNDER_HALF_HOUR);
+          return;
+        }
+
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(durationInMinutes * Fare.CAR_RATE_PER_HOUR / 60);
+                ticket.setPrice(durationInMinutes * Fare.CAR_RATE_PER_HOUR / MINUTES_IN_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(durationInMinutes * Fare.BIKE_RATE_PER_HOUR / 60);
+                ticket.setPrice(durationInMinutes * Fare.BIKE_RATE_PER_HOUR / MINUTES_IN_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
